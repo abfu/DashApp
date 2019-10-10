4f54d1a161a46aa8d8fc98d00a5db69d826bcd8f
Alexander Blaufuss <blaufuss.alexander@gmail.com>
2019-10-10 17:22:04 +0200

    Changed to timeseries dashboard

--- AppTs.py
+++ AppTs.py
@@ -1,44 +1,37 @@
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
-import dash_bootstrap_components as dbc
 import dash_table
 import pandas as pd
 import plotly.graph_objects as go
 import dash_daq as daq
-import plotly.io as pio
+from app import df
 from dash.dependencies import Input, Output, State
 
 
+#%%
+
 df = pd.read_csv('data/stockdata2.csv', index_col=0)
 
-
-# Define dash App
-app = dash.Dash(__name__)
-
-# Define Layout
-
-
+# gets list of stocks to choose from and returns list of dicts with label and value and dict
+# and a dictionary with names
 def get_options(list):
     dict_list = []
     stocknames = {}
     for i in list:
         dict_list.append({'label': i, 'value': i})
+        stocknames[i] = i
 
-    return dict_list
-
-stocknames = {}
-for i in df['stock'].unique():
-    stocknames[i] = i
+    return dict_list, stocknames
 
 
-app.layout = html.Div([
+layout = html.Div([
     html.H1('Stock Prices', style={'textAlign': 'center'}),
-    dcc.Dropdown(id='stockselector', options=get_options(df['stock'].unique()),
+    dcc.Dropdown(id='stockselector', options=get_options(df['stock'].unique()[0]),
                  multi=True, value=['AAPL'], style={'display': 'block', 'margin-left': 'auto',
                  'margin-right': 'auto', 'width': '60%'}
                  ),
-
     dcc.Graph(id='timeseries')
 ], className='container')
 
@@ -48,8 +41,8 @@ def update_graph(selected_dropdown_value):
     dropdown = get_options(df['stock'].unique())[1]
     trace1 = []
     for stock in selected_dropdown_value:
-        trace1.append(go.Scatter(x=df[df['stock'] == stock]['Date'],
-                                 y=df[df['stock'] == stock]['value'],
+        trace1.append(go.Scatter(x=df[df['Stock'] == stock]['Date'],
+                                 y=df[df['Stock'] == stock]['value'],
                                  mode='lines',
                                  opacity=0.7,
                                  name=stock,
@@ -58,13 +51,19 @@ def update_graph(selected_dropdown_value):
     data = [val for sublist in traces for val in sublist]
     figure = {'data': data,
               'layout': go.Layout(
-                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
-              template='plotly_dark'),
-
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056']),
               }
 
     return figure
 
-# Run App
-if __name__ == '__main__':
-    app.run_server(debug=True)
+
+
+
+df = pd.read_csv('data/stockdata2.csv')
+fig = go.Figure()
+for name in pd.unique(df['stock']):
+    fig.add_trace(go.Scatter(x=df[df['stock']==name]['Date'],
+                             y=df[df['stock']==name]['value'],
+                             name=name))
+
+fig.show()
