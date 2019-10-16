eadc114346d21eabea9219298a3ef2d431b05847
Alexander Blaufuss <blaufuss.alexander@gmail.com>
2019-10-16 14:08:41 +0200

    Updated Assets
    Working Dashboard

--- app.py
+++ app.py
@@ -10,14 +10,27 @@ import plotly.io as pio
 from statsmodels.tsa import arima_model
 import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output, State
-from models import figure_model
+from models import figure_transformation
+from flask import Flask, request, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        df = pd.read_csv(request.files.get('file'))
+        return render_template('upload.html', shape=df.shape)
+    return render_template('upload.html')
+
 
 df = pd.read_csv('data/stockdata2.csv', index_col=0, parse_dates=True)
 df.index = pd.to_datetime(df['Date'])
 
 
+
 # Define dash App
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+app = dash.Dash(__name__, meta_tags=[{'name': 'viewport', 'content': 'width=device-width'}])
 
 
 # Define Layout
@@ -51,17 +64,28 @@ app.layout = html.Div(
                             className='div-for-dropdown',
                             children=[
                                 dcc.Dropdown(id='stockselector', options=get_options(df['stock'].unique()),
-                                             multi=True, value=['AAPL']
+                                             multi=True, value=['AAPL'], style={'backgroundColor': '#1E1E1E',
+                                                                                'color': 'red'}, className='stockselector'
                                 ),
                             ],
-                        ),
+                        style={'color': '#1E1E1E'}),
                         # Dropdown for model selection
                         html.Div(
                             className='div-for-dropdown',
                             children=[
-                                dcc.Dropdown(id='modelselector', options=get_options(['30D', 'arima2', 'arima3']),
-                                             multi=False
+                                dcc.Dropdown(id='transformationselector', options=get_options(['Log', 'LogDiff',
+                                                                                               'Diff']),
+                                             multi=False,
+                                             placeholder='Select transformation...',
+                                             style={'backgroundColor': '#1E1E1E'}
                                              ),
+                                dcc.Dropdown(id='periodselector', options=get_options(['1', '2', '3', '4', '5', '6',
+                                                                                       '7', '8', '9', '10', '11' , '12']),
+                                             multi=False,
+                                             placeholder='Select transformation...',
+                                             style={'backgroundColor': '#1E1E1E'}
+                                             ),
+
 
                             ],
                         ),
@@ -71,7 +95,7 @@ app.layout = html.Div(
                 html.Div(
                     className='eight columns div-for-charts bg-grey',
                     children=[
-                            dcc.Graph(id='model', config={'displayModeBar':False}),
+                            dcc.Graph(id='timeseries', config={'displayModeBar': False}),
                             html.Div(
                                 className='text-padding',
                                 children=[
@@ -80,8 +104,9 @@ app.layout = html.Div(
                                                    min=0, max=len(df) - 1, value=[0, len(df) - 1], allowCross=False),
                                 ],
                             ),
-                            dcc.Graph(id='timeseries', config={'displayModeBar': False}),
+
                             dcc.Graph(id='change', config={'displayModeBar': False}),
+                            dcc.Graph(id='transformation', config={'displayModeBar': False}),
                     ],
                 ),
             ],
@@ -155,12 +180,14 @@ def update_change(selected_dropdown_value, value):
 
 
 # Callback from script
-@app.callback(Output('model', 'figure'),
+@app.callback(Output('transformation', 'figure'),
               [Input('stockselector', 'value'),
-               Input('modelselector', 'value'),
-               Input('range', 'value')])
-def update_model(selected_stock, selected_model, value):
-    return figure_model(selected_stock, selected_model, value, df=df)
+               Input('transformationselector', 'value'),
+               Input('range', 'value'),
+               Input('periodselector', 'value')])
+def update_transformation(selected_stock, selected_transformation, value, periods, df=df):
+    return figure_transformation(selected_stock=selected_stock, selected_transformation=selected_transformation,
+                        value=value, periods=periods, df=df)
 
 
 # Run App
--- models.py
+++ models.py
@@ -6,20 +6,43 @@ import numpy as np
 
 
 #%%
-def figure_model(selected_stock, selected_value, value, df):
+
+
+
+def figure_transformation(selected_stock, selected_transformation, value, df, periods=1):
     trace1 = []
     trace2 = []
-    df_sub = df[value[0]:value[1]]
+
+    # Convert str to int
+
+    try:
+        periods = int(periods)
+
+    except:
+        periods = 1
+
+    # drop change
+    df_sub = df[value[0]:value[1]][['value', 'stock']]
+
+
     for stock in selected_stock:
-        df_win = df_sub.loc[df_sub['stock'] == stock].rolling('30D').mean()
+
+        # Depending on selected transformation, return transformed series
+        df_sub_stock = df_sub.loc[df_sub['stock'] == stock]
+        if selected_transformation == 'Log':
+            df_sub_stock['value'] = np.log(df_sub_stock.loc[:, 'value'])
+
+        elif selected_transformation == 'LogDiff':
+            df_sub_stock['value'] = np.log(df_sub_stock.loc[:, 'value']).diff(periods=periods)
+
+        elif selected_transformation == 'Diff':
+            df_sub_stock['value'] = df_sub_stock.loc[:, 'value'].diff(periods=periods)
+
         if value[1] > len(df_sub):
             value[1] = len(df_sub)
-        trace1.append(go.Scatter(x=df_win.index,
-                                 y=np.log(df_win['value']),
+        trace1.append(go.Scatter(x=df_sub_stock.index,
+                                 y=df_sub_stock['value'],
                                  name=stock))
-        #trace2.append(go.Scatter(x=df_win.index,
-         #                        y=ARIMA(np.log(df_win['value']), order=(1, 0, 1)).fit(disp=-1).fittedvalues,
-          #                               name='Arima ' + stock))
     traces = [trace1, trace2]
     data = [val for sublist in traces for val in sublist]
     figure = {'data': data,
@@ -30,6 +53,35 @@ def figure_model(selected_stock, selected_value, value, df):
                       plot_bgcolor='rgba(0, 0, 0, 0)',
                   hovermode='x',
               ),
+              }
+
+    return figure
+
+
+
+def figure_graph(selected_dropdown_value, value):
+    trace1 = []
+    df_sub = df[value[0]:value[1]]
+    for stock in selected_dropdown_value:
+        trace1.append(go.Scatter(x=df_sub[df_sub['stock'] == stock].index,
+                                 y=df_sub[df_sub['stock'] == stock]['value'],
+                                 mode='lines',
+                                 opacity=0.7,
+                                 name=stock,
+                                 textposition='bottom center'))
+    traces = [trace1]
+    data = [val for sublist in traces for val in sublist]
+    figure = {'data': data,
+              'layout': go.Layout(
+                  colorway=["#5E0DAC", '#FF4F00', '#375CB1', '#FF7400', '#FFF400', '#FF0056'],
+                  template='plotly_dark',
+                               paper_bgcolor='rgba(0, 0, 0, 0)',
+                  plot_bgcolor='rgba(0, 0, 0, 0)',
+                  margin={'b': 15},
+                  hovermode='x',
+                  autosize=True
+            ),
+
               }
 
     return figure
